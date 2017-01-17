b7d08cc5d0f0e054e394d3f5f2a51755b3025338
BuildTools <moggy1972@gmail.com>
2017-01-17 17:51:23 +0000

    testing 2

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 // moggy test
+//branch2
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
